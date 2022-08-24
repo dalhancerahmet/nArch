eadc71db10be8c6c49950342d42855d6b4b1772d
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-08-24 14:35:00 +0300

    24/08/2022-2

--- WebAPI/Controllers/BrandsController.cs
+++ WebAPI/Controllers/BrandsController.cs
@@ -12,6 +12,7 @@ namespace WebAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
         {
+            //Add metoduna bir istek geldiği zaman MEdiator.Send ile ilgili class'a(createBrandCommand) sevk ediyor(Gönderiyor/yolluyor).
            CreatedBrandDto results= await Mediator.Send(createBrandCommand);
             return Created("",results);
         }
